6d89e96d3db503d56532dfd509da393a09588b3f
Milandeep Bassi <milandeepbassi@gmail.com>
2020-01-16 10:01:21 +0000

    Created stateless widget which is immutable. Generated a State class inside the widget to allow for randomPair generation. Added new build method to Randomly generate words. Implemented the itemBuilder to generate a list of words while scrolling. Used Scaffold to generate a template structure for the app.

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+  Widget build(BuildContext context) {/*
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -19,9 +18,59 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           //child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
+      ),
+    );*/
+    return MaterialApp(
+      title: "Start up Name Generator",
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    //final WordPair wordPair = WordPair.random();
+    //return Text(wordPair.asPascalCase);
+    return Scaffold (                   // Add from here...
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
